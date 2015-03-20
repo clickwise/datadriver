194cf4ccf2f4de5670f837d382ec6e19943cded9
clickwise <strongmind666@126.com>
2015-03-20 12:07:19 +0800

    command and response abstract

--- src/cn/clickwise/datadriver/rpc/Command.java
+++ src/cn/clickwise/datadriver/rpc/Command.java
@@ -1,5 +1,19 @@
 package cn.clickwise.datadriver.rpc;
 
-public interface Command {
+import java.io.Serializable;
 
+/**
+ *  the abstract of command executed in the remote machine
+ * @author lq
+ */
+
+@SuppressWarnings("serial")
+public abstract class Command implements Serializable{
+	
+	/**
+	 * execute the command ,write output to Response object
+	 * @return Response
+	 */
+	public abstract Response execute();
+	
 }
---
+++ src/cn/clickwise/datadriver/rpc/Response.java
@@ -0,0 +1,20 @@
+package cn.clickwise.datadriver.rpc;
+
+import java.io.Serializable;
+
+/**
+ * the output of executing a command in remote machine
+ * @author lq
+ */
+
+@SuppressWarnings("serial")
+public abstract class Response implements Serializable{
+
+	/**
+	 * explain the Response object in the local machine
+	 * write result values to stdout
+	 * write result files to local filesystem
+	 */
+	public abstract void flush();
+	
+}
